920ca5bf35f6331eb50b7d16d1ad6112ddd5bfe7
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-09-23 15:49:50 -0500

    router historias corregido

--- routes/historias.js
+++ routes/historias.js
@@ -13,10 +13,4 @@ router.post('/', crearHistoria);
 router.put('/:id', auth.requerido,modificarHistoria);
 router.delete('/:id', auth.requerido,eliminarHistoria);
 
-router.get('/', auth.requerido, obtenerUsuario)
-router.get('/:id', auth.requerido, obtenerUsuarios);
-router.post('/', crearUsuario)
-router.post('/entrar', iniciarSesion)
-router.put('/:id', auth.requerido, modificarUsuario)
-router.delete('/:id', auth.requerido, eliminarUsuario)
 module.exports = router;
