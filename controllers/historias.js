e247a7dc044f3556e7b903f888400f4678d2c253
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-09-23 15:27:49 -0500

    controladores historia y usuario

--- controllers/historias.js
+++ controllers/historias.js
@@ -2,41 +2,17 @@ const Historia = require("../models/Historia");
 
 // CRUD
 function crearHistoria(req, res) {
-  let { titulo, texto, id_Historia, tags, tematica, ficcion } = req.body;
-  let historia = new Historia(
-    titulo,
-    texto,
-    id_Historia,
-    tags,
-    tematica,
-    ficcion
-  );
-  res.send(historia);
+  const body = req.body,
+  const historia = new Historia(body)
+  historia.save().then(his => {                                         //Guardando nuevo usuario en MongoDB.
+    return res.status(201).json(updated.publicData())
+  }).catch(next)
 }
 
 function obtenerHistorias(req, res) {
-  let historia1 = new Historia(
-    "El picador criminal mutilador",
-    `Dolor non commodo tempor ea culpa tempor eiusmod proident sint Lorem adipisicing. Aliqua laboris Lorem proident laboris est officia deserunt ipsum. Ut sint laborum proident duis reprehenderit ullamco dolor Lorem nulla aute sit. Ea aliquip anim velit id non aliqua amet non ea quis mollit. Consectetur ipsum voluptate officia reprehenderit qui. Nostrud esse cillum dolor magna labore non tempor excepteur. Consequat exercitation nisi incididunt cupidatat adipisicing excepteur.
-    Adipisicing eiusmod minim aute enim voluptate cillum aliqua deserunt Lorem consectetur ipsum sunt. Velit commodo dolor aliquip officia velit fugiat et. Consectetur consequat ea voluptate incididunt aliqua pariatur ad. Elit cillum irure sit sunt est laborum.
-    Sit ea veniam quis nostrud non incididunt deserunt dolore cupidatat. Laboris culpa voluptate mollit enim ad anim aliquip eu enim quis excepteur. Tempor occaecat velit laborum sunt. Nulla velit id commodo incididunt labore. Ullamco sit minim cillum cupidatat cillum et excepteur ea mollit non enim. Pariatur fugiat tempor quis id mollit exercitation commodo velit in ipsum cillum. Esse reprehenderit deserunt do ea pariatur sit nulla sunt aute.
-    Exercitation qui sit nostrud enim esse. Fugiat nostrud Lorem proident voluptate ea Lorem est reprehenderit eu irure proident. Amet cillum deserunt laboris aliquip enim cupidatat anim excepteur sit. Exercitation in excepteur do minim laboris non irure deserunt irure sint consequat reprehenderit nisi. Lorem amet occaecat veniam elit minim exercitation magna in non pariatur reprehenderit. Labore deserunt et amet enim ipsum magna reprehenderit pariatur est ut pariatur.`,
-    7,
-    ["paranormal", "noche", "nickelodeon"],
-    "Paranormal",
-    true
-  );
-  let historia2 = new Historia(
-    "Leyenda de la Llorona",
-    `Cupidatat culpa minim exercitation magna magna ad non ex. Minim aute anim eiusmod cillum enim minim. Voluptate sit cupidatat tempor reprehenderit commodo adipisicing nostrud quis deserunt voluptate magna aliquip. Ad cupidatat eiusmod veniam eu nostrud. Voluptate ea dolore exercitation duis tempor aliqua nisi eiusmod. Excepteur Lorem Lorem adipisicing irure mollit.
-    Anim velit duis Lorem nulla tempor. Ullamco culpa in nisi quis eu et pariatur voluptate ex aliquip ex. Voluptate non exercitation proident dolore veniam adipisicing tempor mollit nisi sint adipisicing sunt minim. Consectetur exercitation consectetur dolore dolore occaecat velit laboris exercitation in proident reprehenderit occaecat. Nostrud amet tempor pariatur quis Lorem dolore ullamco in laborum eiusmod do anim pariatur. Voluptate esse fugiat deserunt est mollit elit sit eiusmod. Id excepteur qui velit excepteur aute cupidatat consectetur culpa deserunt sint.
-    Ad ea magna nisi voluptate pariatur irure. Quis cupidatat commodo tempor minim ut aliqua velit dolore aliqua excepteur sint. Consectetur dolore in non duis sint pariatur incididunt pariatur irure excepteur id dolor cillum. Eu mollit tempor deserunt tempor culpa anim aute tempor. Aliqua veniam laborum deserunt anim sint fugiat anim cupidatat incididunt tempor et excepteur sunt. In culpa aliqua est ex voluptate est nulla amet laboris labore ea excepteur qui aute. Aliquip mollit cupidatat aliquip pariatur.`,
-    8,
-    ["leyenda", "regional", "paranormal", "fantasmas"],
-    "Leyendas",
-    false
-  );
-  res.send([historia1, historia2]);
+  Historia.find(req.params.titulo)
+  .then(uss => res.status(200).send(uss))
+  .catch(next);
 }
 
 function modificarHistoria(req, res) {
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,56 +1,64 @@
-const { json } = require('body-parser');
-const mongoose = require('mongoose');
+const { json } = require("body-parser");
+const mongoose = require("mongoose");
 const Usuario = mongoose.model("Usuario");
 
 /* CRUD */
 
 function crearUsuario(req, res, next) {
-    const nuevoUsuario = new Usuario(req.body);
-    nuevoUsuario.save()
-    .then(us => res.status(200).send(us))
+  const body = req.body,
+    password = body.password;
+  delete body.password;
+  const usuario = new Usuario(body);
+  usuario.crearPassword(password);
+  usuario
+    .save()
+    .then((user) => {
+      //Guardando nuevo usuario en MongoDB.
+      return res.status(201).json(user.toAuthJSON());
+    })
     .catch(next);
 }
 
 function obtenerUsuario(req, res, next) {
-    Usuario.findById(req.params.id)
-    .then(us => res.send(us))
+  Usuario.findById(req.params.id)
+    .then((us) => res.send(us))
     .catch(next);
 }
 
 function obtenerUsuarios(req, res, next) {
-    Usuario.find()
-    .then(uss => res.status(200).send(uss))
+  Usuario.find()
+    .then((uss) => res.status(200).send(uss))
     .catch(next);
 }
 function modificarUsuario(req, res, next) {
-    Usuario.findById(req.params.id)
-    .then(us => {
-        if(!us) return res.sendStatus(404);
-        const nuevaInfo = req.body;
-        const nuevaInfoKeys = Object.keys(nuevaInfo);
-        for (let i = 0; i < nuevaInfoKeys.length; i++) {
-            if(typeof us[nuevaInfoKeys[i]] !== "undefined"){
-                console.log(us[nuevaInfoKeys[i]]);
-                us[nuevaInfoKeys[i]] = nuevaInfo[nuevaInfoKeys[i]];
-            } else continue;
-        }
-        us.save()
-        .then(updated => res.status(201).json(updated.publicData()))
+  Usuario.findById(req.params.id)
+    .then((us) => {
+      if (!us) return res.sendStatus(404);
+      const nuevaInfo = req.body;
+      const nuevaInfoKeys = Object.keys(nuevaInfo);
+      for (let i = 0; i < nuevaInfoKeys.length; i++) {
+        if (typeof us[nuevaInfoKeys[i]] !== "undefined") {
+          console.log(us[nuevaInfoKeys[i]]);
+          us[nuevaInfoKeys[i]] = nuevaInfo[nuevaInfoKeys[i]];
+        } else continue;
+      }
+      us.save()
+        .then((updated) => res.status(201).json(updated.publicData()))
         .catch(next);
     })
     .catch(next);
 }
 
 function eliminarUsuario(req, res, next) {
-    Usuario.findOneAndDelete({_id:req.params.id})
-    .then(r => res.status(200).send('El usuario ha sido eliminado'))
+  Usuario.findOneAndDelete({ _id: req.params.id })
+    .then((r) => res.status(200).send("El usuario ha sido eliminado"))
     .catch(next);
 }
 
 module.exports = {
-    crearUsuario,
-    obtenerUsuario,
-    obtenerUsuarios,
-    modificarUsuario,
-    eliminarUsuario
-}
+  crearUsuario,
+  obtenerUsuario,
+  obtenerUsuarios,
+  modificarUsuario,
+  eliminarUsuario,
+};
