b03666dda190e394fa265a5333341b489c31072a
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-09-23 15:40:19 -0500

    app js

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ app.use(bodyParser.json());
 // Configuración de base de datos
 const mongoose = require('mongoose');
 
+// mongoose.connect(
+// 	process.env.MONGODB_URI,
+//     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+// 	);
 mongoose.connect(
-	process.env.MONGODB_URI,
+	"mongodb+srv://<username>:<password>@cluster0.fncuh.mongodb.net/test",
     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 	);
-
+    
 mongoose.set("debug", true)
 
 require('./models/Usuario')
@@ -24,6 +28,10 @@ require('./models/Historia')
 app.use('/v1', require('./routes'));
 
 // Inicializando el servidor
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on ${process.env.PORT}`);
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is listening on ${process.env.PORT}`);
+// });
+app.listen(4001, () => {
+    console.log(`Server is listening on 4001`);
 });
