56d13eab0ed69a0e4426f50d38a0ba210762fcde
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-09-23 15:10:38 -0500

    modelos historia y usuario

--- models/Historia.js
+++ models/Historia.js
@@ -10,3 +10,40 @@ class Historia {
 }
 
 module.exports = Historia;
+
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema(
+  {
+    titulo: {
+      type: String,
+      unique: true, //este campo no se puede repetir
+      required: [true, "no puede estar vacío"],
+      index: true,
+    },
+    tags: {
+      type: Array
+    },
+    id_usuario: { type: String, required: true },
+    texto: String,
+    tematica: String,
+    ficcion: Boolean,
+    url_origen: Boolean,
+  },
+  { collection: "historias", timestamps: true }
+);
+
+UsuarioSchema.methods.publicData = () => {
+  return {
+    id: this._id,
+    titulo: this.titulo,
+    tags: this.tags,
+    id_usuario: this.id_usuario,
+    texto: this.texto,
+    tematica: this.tematica,
+    ficcion: this.ficcion,
+    url_origen: this.url_origen
+  };
+};
+
+mongoose.model("Usuario", UsuarioSchema);
