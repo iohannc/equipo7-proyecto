c692d121d514081b52cbcd291742de9b70c29a0a
HugoJosh <80298571+HugoJosh@users.noreply.github.com>
2021-10-27 16:58:16 -0500

    Update usuarios.js

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -124,33 +124,23 @@ function eliminarUsuario(req, res, next) {
 
 function iniciarSesion(req, res, next) {
   if (!req.body.email) {
-    return res
-      .status(422)
-      .json({ errors: { email: "El campo email no puede estar vacío." } });
+    return res.status(422).json({ errors: { email: "no puede estar vacío" } });
   }
 
   if (!req.body.password) {
-    return res.status(422).json({
-      errors: { password: "El campo password no puede estar vacío." },
-    });
+    return res.status(422).json({ errors: { password: "no puede estar vacío" } });
   }
 
-  passport.authenticate(
-    "local",
-    { session: false },
-    function (err, user, info) {
-      if (err) {
-        return next(err);
-      }
+  passport.authenticate('local', { session: false }, function (err, user, info) {
+    if (err) { return next(err); }
 
-      if (user) {
-        user.token = user.generarJWT();
-        return res.json({ user: user.toAuthJSON() });
-      } else {
-        return res.status(422).json(info);
-      }
+    if (user) {
+      user.token = user.generarJWT();
+      return res.json({ user: user.toAuthJSON() });
+    } else {
+      return res.status(422).json(info);
     }
-  );
+  })(req, res, next);
 }
 
 module.exports = {
